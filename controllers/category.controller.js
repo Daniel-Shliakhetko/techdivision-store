b0a5b140e3932a655992c0ccf8e49014af9d296a
Daniel-Shliakhetko <danielshliakhetko@gmail.com>
2022-08-26 16:49:52 +0300

    Added getCategoryByPath, getCategoryChildrens and GetCategoryParent

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -31,10 +31,10 @@ const postCategory = async (req, res) => {
   }
 };
 
-const getCategory = async (req, res) => {
+const getCategory = (req, res) => {
   const id = req.params.id;
 
-  Category.findById()
+  Category.findById(id)
     .then((data) => {
       res.status(201).json(data);
     })
@@ -43,7 +43,55 @@ const getCategory = async (req, res) => {
     });
 };
 
+const getCategoryByPath = (req, res) => {
+  const slug = "/" + req.params.parent;
+  const children = req.params.children ? "/" + req.params.children : "";
+
+  Category.findOne({ category: slug + children })
+    .then((data) => {
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Something went wrong" });
+    });
+};
+
+const getCategoryChlidrens = async (req, res) => {
+  const parent = "/" + req.params.parent;
+  const children = req.params.children ? "/" + req.params.children : "";
+
+  const childrens = await Category.find({ parent: parent + children });
+
+  if (childrens) {
+    res.status(201).json(childrens);
+  } else {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+const getCategoryParent = async (req, res) => {
+  const slug = "/" + req.params.parent;
+  const children = req.params.children ? "/" + req.params.children : "";
+
+  const object = await Category.findOne({ category: slug + children });
+
+  if (object) {
+    Category.findOne({ category: object.parent })
+      .then((data) => {
+        res.status(201).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: "Something went wrong" });
+      });
+  } else {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
 module.exports = {
   postCategory,
   getCategory,
+  getCategoryByPath,
+  getCategoryChlidrens,
+  getCategoryParent
 };
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -4,9 +4,24 @@ const router = express.Router();
 const {
   postCategory,
   getCategory,
+  getCategoryByPath,
+  getCategoryChlidrens,
+  getCategoryParent
 } = require("../controllers/category.controller");
 
 router.post("/add", postCategory);
-router.get("/get/:id", getCategory);
+router.get("/get/id/:id", getCategory);
+router.get(
+  ["/get/:parent/childrens", "/get/:parent/:children/childrens"],
+  getCategoryChlidrens
+);
+router.get(
+    ["/get/:parent/parent", "/get/:parent/:children/parent"],
+    getCategoryParent
+  );
+router.get(
+  ["/get/path/:parent", "/get/path/:parent/:children"],
+  getCategoryByPath
+);
 
 module.exports = router;
